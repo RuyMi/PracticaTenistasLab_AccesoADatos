3d3ad9d904dc530ca122d2a3d88c25a0305f3698
Rubén García-Redondo Marín <rubengrm@icloud.com>
2022-12-03 22:39:48 +0100

    Añadido servicio de cifrado de password + controlador

--- src/main/kotlin/models/Tarea.kt
+++ src/main/kotlin/models/Tarea.kt
@@ -4,8 +4,8 @@ import models.enums.TipoEncordado
 import models.enums.TipoNudos
 import java.util.*
 
-sealed class Tarea(){
-    data class Encordado(
+data class Tarea(
+
         val uuid: UUID,
         val raqueta: List<Producto>,
         val precio: Double,
@@ -13,17 +13,3 @@ sealed class Tarea(){
         val nudos: TipoNudos
     ){}
 
-    data class Personalizacion(
-        val uuid: UUID,
-        val raqueta: List<Producto>,
-        val precio: Double,
-        val peso: Double,
-        val balance: Double,
-        val rigidez: Double
-    )
-
-    data class Adquisicion(
-        val uuid: UUID,
-        val precio: Double,
-    )
-}
