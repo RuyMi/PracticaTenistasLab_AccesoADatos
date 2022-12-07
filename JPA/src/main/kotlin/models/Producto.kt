8d09fd2735b1e7da871295ede60e3474b6b6f742
Alejandro <alexlopezabad13@gmail.com>
2022-12-07 18:27:05 +0100

    JPA MODELS

---
+++ JPA/src/main/kotlin/models/Producto.kt
@@ -0,0 +1,42 @@
+package models
+
+import kotlinx.serialization.Serializable
+import org.hibernate.annotations.Type
+import serializers.UUIDSerializer
+import java.util.*
+import javax.persistence.*
+
+
+/**
+ * Producto
+ *
+ * @property id
+ * @property uuid
+ * @property marca
+ * @property modelo
+ * @property precio
+ * @property stock
+ * @constructor Create empty Producto
+ */
+@Serializable
+@Entity
+@Table(name = "Productos")
+@NamedQueries(
+    NamedQuery(name = "Productos.findAll", query = "SELECT t FROM Producto t"),
+    NamedQuery(
+        name = "Producto.porUUID",
+        query = "SELECT t FROM Producto t WHERE t.uuid = t"
+    ),
+)
+data class Producto(
+    @Id @GeneratedValue
+    val id:Int,
+    @Serializable(UUIDSerializer::class)
+    @Column(name="UUID_Producto")
+    @Type(type = "uuid-char")
+    val uuid: UUID = UUID.randomUUID(),
+    val marca: String,
+    val modelo: String,
+    val precio: Double,
+    val stock: Int
+)
---
+++ JPA/src/main/kotlin/models/Turno.kt
@@ -0,0 +1,42 @@
+package models
+
+import kotlinx.serialization.Serializable
+import org.hibernate.annotations.Type
+import serializers.LocalDateTimeSerializer
+import serializers.UUIDSerializer
+import java.time.LocalDateTime
+import java.util.*
+import javax.persistence.*
+
+/**
+ * Turno
+ *
+ * @property id
+ * @property uuidTurno
+ * @property fechaInicio
+ * @property fechaFin
+ * @constructor Create empty Turno
+ */
+@Serializable
+@Entity
+@Table(name = "Turnos")
+@NamedQueries(
+    NamedQuery(name = "Turnos.findAll", query = "SELECT t FROM Turno t"),
+    NamedQuery(
+        name = "Turno.porUUID",
+        query = "SELECT t FROM Turno t WHERE t.uuidTurno = t"
+    ),
+)
+data class Turno(
+    @Id @GeneratedValue
+    val id: Int,
+    @Serializable(UUIDSerializer::class)
+    @Column(name="UUID_Turno")
+    @Type(type = "uuid-char")
+    val uuidTurno: UUID,
+    @Serializable(LocalDateTimeSerializer::class)
+    val fechaInicio: LocalDateTime,
+    @Serializable(LocalDateTimeSerializer::class)
+    val fechaFin: LocalDateTime
+) {
+}
